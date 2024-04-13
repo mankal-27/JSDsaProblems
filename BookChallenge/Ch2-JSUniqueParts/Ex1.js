5e46bce140d7a758039aea73ebe4512a3d14cdeb
mankal-27 <mnkalkutagi@gmail.com>
2024-04-13 13:18:17 +0530

    JsObjexts

---
+++ BookChallenge/Ch2-JSUniqueParts/Ex1.js
@@ -0,0 +1,19 @@
+function isEquivalent(a,b){
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    if(aProps.length !== bProps.length){
+        return false
+    }
+
+    for(var i =0; i< aProps.length; i++){
+        var propName = aProps[i]
+
+        if(a[propName] !== b[propName]){
+            return false
+        }
+    }
+    return true
+}
+
+isEquivalent({'hi':12}, {'hi':12})
